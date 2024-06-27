c98fb62e48d1b8a317d72573087c541abe272b80
iamjai-3 <jai.develop3r@gmail.com>
2024-06-27 19:39:58 +0530

    added-nx

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { add } from '@boilerplate/sample-lib';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('add', add(5, 2));
   await app.listen(3000);
 }
 bootstrap();
